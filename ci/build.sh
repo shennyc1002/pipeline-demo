e77a9a25c89fbcc2ba47ebf9a3de10ed8958e682
shennyc1002 <shennyc1002@gmail.com>
2019-01-18 16:44:52 -0600

    added chmod for gradlew

--- ci/build.sh
+++ ci/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 set -e
-chmod +x pipeline-demo/gradlew --build-file pipeline-demo/build.gradle build
+pipeline-demo/gradlew --build-file pipeline-demo/build.gradle build
 cp pipeline-demo/build/libs/*.jar build-output/
